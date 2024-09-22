8fe1e271855fa31695d817988db0286efc72dcb5
Marciamallowxim <110351665+Marciamallowxim@users.noreply.github.com>
2024-09-22 22:00:35 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,2 @@
-python3.12 -m venv venv
-
-# activate the virtual environment
-source venv/bin/activate
-
-# install all deps in the venv
 pip install -r requirements.txt
-
-# collect static files using the Python interpreter from venv
-python manage.py collectstatic --noinput
+python3.9 manage.py collectstatic
